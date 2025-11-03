213371a4e5c703df26431205d81cf6b2cfe6ded2
Owen Sweet <o.sweet.work@gmail.com>
2025-11-02 23:05:35 -0800

    Made sameSite lax so that cross site requests can be made

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -61,7 +61,7 @@ router.post('/register', registerValidation, async (req, res) => {
 
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'lax',
             maxAge: 24 * 60 * 60 * 1000, // 24 hours
         });
 
